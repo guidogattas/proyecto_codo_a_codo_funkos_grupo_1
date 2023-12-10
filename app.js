e017caebb7c3c7e98b1cd15687b7ea31e066bdc1
Guido Gattas <guido.gattas@gmail.com>
2023-12-10 18:10:48 -0300

    glide slide

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const mainRoutes = require('./src/routes/mainRoutes')
 const shopRoutes = require('./src/routes/shopRoutes')
 const adminRoutes = require('./src/routes/adminRoutes')
