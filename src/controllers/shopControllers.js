fcfdb85a9e4cbff1759521cf01017419685be5cc
Guido Gattas <guido.gattas@gmail.com>
2023-12-13 21:08:26 -0300

    [FIX] shopController

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -3,14 +3,7 @@ const funkoData = require('../loaders/funkoLoader')
 
 const shopControllers = {
     shopView: (req, res) => {
-        let filteredCollection;
-        if (req.params.collectionName) {
-            const collectionName = req.params.collectionName;
-            filteredCollection = funkoData.filter(item => item.collection_name === collectionName);
-        } else {
-            filteredCollection = funkoData;
-        }
-        res.render('pages/shop.ejs', { collection: filteredCollection });
+        res.render('pages/shop.ejs', { collection: funkoData });
     },
     itemView: (req, res) => {
         const id = req.params.id;
