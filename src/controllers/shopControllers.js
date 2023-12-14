b8760f89b009deb3ecf8bbb7abd366aaec1fbdac
Guido Gattas <guido.gattas@gmail.com>
2023-12-13 21:44:07 -0300

    [FIX] shopController

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -3,18 +3,19 @@ const funkoData = require('../loaders/funkoLoader')
 
 const shopControllers = {
     shopView: (req, res) => {
-        res.render('pages/shop.ejs', { collection: funkoData });
+        let filteredCollection;
+        if (req.params.collectionName) {
+            const collectionName = req.params.collectionName;
+            filteredCollection = funkoData.filter(item => item.collection_name === collectionName);
+        } else {
+            filteredCollection = funkoData;
+        }
+        res.render('pages/shop.ejs', { collection: filteredCollection });
     },
     itemView: (req, res) => {
         const id = req.params.id;
         const item = funkoData.find(item => item.product_id == id);
         res.render('pages/item.ejs', { item: item, funkoData: funkoData })
-    },
-    collection: (req, res) => {
-        const collectionName = req.params.collectionName;
-        const filteredCollection = funkoData.filter(item => item.collection_name === collectionName);
-
-        res.render('pages/collection.ejs', { collection: filteredCollection });
     },
     addItem: (req, res) => res.send('Route to add the current item to the shop cart'),
     cartView: (req, res) => {
